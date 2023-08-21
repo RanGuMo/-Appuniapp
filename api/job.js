ca76449d7439a1c46cbe8fbe7264853dd9cdc799
MoRanGu <1126146235@qq.com>
2023-08-21 23:32:18 +0800

    company页面的 列表渲染以及下拉加载更多

--- api/job.js
+++ api/job.js
@@ -11,12 +11,22 @@ export const categoryGet = ()=>{
 }
 
 //岗位
-export const jobGet = (page=1)=>{
+export const jobGet = (page=1,name)=>{
 	let skip = (page-1)*10
+	let where = {}
+	if(name){
+		where.lv1 = name
+	}
 	return request.get('classes/job',{
 		params:{
 			limit:10,
-			skip
+			skip,
+			where
 		}
 	})
+}
+
+//岗位详情
+export const jobDetailGet = (id)=>{
+	return request.get(`classes/job/${id}`)
 }
--- pages/company/company.vue
+++ pages/company/company.vue
@@ -1,18 +1,51 @@
 <template>
   <view>
-    
+    <view class="padding-sm flex flex-wrap justify-between">
+      <job-item v-for="item in jobList" :jobdata="item" />
+    </view>
   </view>
 </template>
 
 <script>
+  import {
+    jobGet
+  } from '../../api/job'
   export default {
     data() {
       return {
-        
+        jobList: [],
+        page: 1
       }
     },
+    onLoad() {
+      jobGet().then(res => {
+        this.jobList = res.data.results
+        this.page++
+      })
+    },
+    // 下拉加载更多内容，触底就弹出提示
+    onReachBottom() {
+      console.log('触底了');
+      jobGet(this.page).then(res => {
+        let {
+          results
+        } = res.data
+        if (results.length) {
+          this.jobList = [
+            ...this.jobList,
+            ...results
+          ]
+          this.page++
+          return
+        }
+        uni.showToast({
+          title: "没有更多数据了",
+          icon: 'none'
+        })
+      })
+    },
     methods: {
-      
+
     }
   }
 </script>
