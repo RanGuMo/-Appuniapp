f5717c4d6d839ee5e86e9ca1f998ae5d8e2e01b8
MoRanGu <1126146235@qq.com>
2023-09-12 10:32:44 +0800

    岗位详情页面搭建

---
+++ pages/detail/detail.vue
@@ -0,0 +1,75 @@
+<template>
+  <view>
+    <view class="text-center">
+      <image class="cu-avatar round xl margin-tb" src="../../static/img/avatar1.png" mode=""></image>
+      <view class="text-lg">
+        前端开发工程师
+      </view>
+      <view class="text-orange margin-tb-sm">
+        薪资待遇：10k
+      </view>
+    </view>
+    <view class="flex justify-around margin-tb">
+      <view class="">
+        岗位详情
+      </view>
+      <view class="">
+        企业信息
+      </view>
+    </view>
+    
+    <view class="padding">
+      <view class="margin-bottom-sm">
+        经验要求
+      </view>
+      具体经验要求
+    </view>
+    
+    <view class="padding">
+      <view class="margin-bottom-sm">
+        技能要求
+      </view>
+      具体要求
+    </view>
+    
+    <view class="padding">
+      <view class="margin-bottom-sm">
+        福利待遇
+      </view>
+      具体福利待遇
+    </view>
+    
+    <view class="padding">
+      <view class="margin-bottom-sm">
+        公司位置
+      </view>
+      地图组件
+    </view>
+    
+    <view class="padding cu-bar foot">
+      <button class="cu-btn bg-orange lg block">立即报名</button>
+    </view>
+    
+    
+    
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+.lg {
+  width: 100%;
+}
+</style>
